378899484dc6b7551e047fbcf63e2092581a3be5
gloriaaa0312 <90241912+gloriaaa0312@users.noreply.github.com>
2022-11-25 23:29:36 +0800

    Add files via upload

--- be/model/store.py
+++ be/model/store.py
@@ -14,32 +14,104 @@ class Store:
         try:
             conn = self.get_db_conn()
             conn.execute(
-                "CREATE TABLE IF NOT EXISTS user ("
-                "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
-                "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
+                "CREATE TABLE IF NOT EXISTS User("
+                "user_name TEXT PRIMARY KEY,"  # 用户名
+                "pwd varchar(15) ,"  # 密码
+                "balance INTEGER,"  # 余额
+                "token TEXT,"  # token
+                "terminal TEXT"  # 端口
+                ")"
             )
+            conn.execute("INSERT INTO User values(?,?,?,?,?)",
+                         ('test_id', 'test_pwd', 100, 'test_token', 'test_terminal'))
+            conn.execute("INSERT INTO User values(?,?,?,?,?)",
+                         ('test_id_2', 'test_pwd_2', 1000, 'test_token_2', 'test_terminal_2'))
 
             conn.execute(
-                "CREATE TABLE IF NOT EXISTS user_store("
-                "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
+                "CREATE TABLE IF NOT EXISTS Book( "
+                "book_id TEXT PRIMARY KEY,"  # 书本编号
+                "title TEXT,"  # 书名
+                "author TEXT,"  # 书本信息
+                "publisher TEXT,"
+                "original_title TEXT,"
+                "translator TEXT,"
+                "pub_year TEXT,"
+                "pages INTEGER,"
+                "price INTEGER,"
+                "currency_unit TEXT,"
+                "binding TEXT,"
+                "isbn TEXT,"
+                "author_intro TEXT,"
+                "book_intro text,"
+                "content TEXT,"
+                "tags TEXT,"
+                "picture BLOB"
+                ")"
             )
+            conn.execute("INSERT INTO Book values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (
+                'book1', '霸王别姬', '李碧华', '新星出版社', '无', '无', '1', 235, 30, '1', '1', '1', '1', '1', '1', '1', '1'))
 
             conn.execute(
-                "CREATE TABLE IF NOT EXISTS store( "
-                "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
-                " PRIMARY KEY(store_id, book_id))"
+                "CREATE TABLE IF NOT EXISTS Store("
+                "store_id INTEGER PRIMARY KEY,"  # 店铺编号
+                "owner TEXT,"  # 卖家user_name
+                "FOREIGN KEY(owner) REFERENCES User(user_name)"
+                ")"
             )
+            conn.execute("INSERT INTO Store values(?,?)", (1, 'test_id'))
 
             conn.execute(
-                "CREATE TABLE IF NOT EXISTS new_order( "
-                "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
+                "CREATE TABLE IF NOT EXISTS Books_in_store( "
+                "store_id INTEGER,"  # 店铺编号
+                "book_id TEXT,"  # 书本编号
+                "book_name TEXT,"  # 书名
+                "book_num INTEGER,"  # 在售数量，完成一次订单后数量-1
+                "FOREIGN KEY(store_id) REFERENCES Store(store_id),"
+                "FOREIGN KEY(book_id) REFERENCES Book(book_id),"
+                "FOREIGN KEY(book_name) REFERENCES Book(title),"
+                "PRIMARY KEY(store_id, book_id)"
+                ")"
             )
+            conn.execute("INSERT INTO Books_in_store values(?,?,?,?)", (1, 'book1', '霸王别姬', 10))
 
             conn.execute(
-                "CREATE TABLE IF NOT EXISTS new_order_detail( "
-                "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
-                "PRIMARY KEY(order_id, book_id))"
+                "CREATE TABLE IF NOT EXISTS Orders( "
+                "order_id INTEGER PRIMARY KEY,"  # 订单编号
+                "seller TEXT,"  # 卖家
+                "buyer TEXT,"  # 买家
+                "status INTEGER,"  # 0为未付款（*这里可以不要 如果我们后面写的是点击购买后直接扣款就直接进入状态1），
+                # 1为以付款未发货（买家付款后，商家填入物流前），
+                # 2为已发货未收货（商家填入物流后，买家确认收货前），3为已收货完成订单 
+                "time TEXT,"  # 下单时间，格式为 "YYYY-MM-DD HH:MM:SS.SSS" 的日期
+                "book_id TEXT,"  # 书本编号
+                "price INTEGER,"  # 书本价格
+                "FOREIGN KEY(seller) REFERENCES Store(owner),"
+                "FOREIGN KEY(buyer) REFERENCES User(user_name),"
+                "FOREIGN KEY(book_id) REFERENCES Book(book_id)"
+                ")"
             )
+            conn.execute("INSERT INTO Orders values(?,?,?,?,?,?,?)",
+                         (1, 'test_id', 'test_id_2', 3, '2022-11-25 23:00:50.000', 'book1', 10))
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS User_order("
+                "user_name TEXT,"  # 用户名
+                "order_id INTEGER PRIMARY KEY,"  # 订单号
+                "FOREIGN KEY(user_name) REFERENCES User(user_name),"
+                "FOREIGN KEY(order_id) REFERENCES Orders(order_id)"
+                ")"
+            )
+            conn.execute("INSERT INTO User_order values(?,?)", ('test_id', 1))
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS User_store("
+                "user_name TEXT,"  # 用户名
+                "store_id INTEGER PRIMARY KEY,"  # 店铺号
+                "FOREIGN KEY(user_name) REFERENCES User(user_name),"
+                "FOREIGN KEY(store_id) REFERENCES Store(store_id)"
+                ")"
+            )
+            conn.execute("INSERT INTO User_store values(?,?)", ('test_id', 1))
 
             conn.commit()
         except sqlite.Error as e:
